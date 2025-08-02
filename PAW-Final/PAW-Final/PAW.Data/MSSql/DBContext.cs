04a36305812b0149f658f4c14a08809ac0ed9f02
Maria Celeste Solano Hidalgo <ms2307628@gmail.com>
2025-08-01 18:49:19 -0600

    Proyecto

---
+++ PAW-Final/PAW-Final/PAW.Data/MSSql/DBContext.cs
@@ -0,0 +1,20 @@
+﻿using PAW.Models.Entities;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Xml.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace PAW.Data.MSSql
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Board> Boards { get; set; }
+        public DbSet<List> Lists { get; set; }
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+    }
+}
---
+++ PAW-Final/PAW-Final/PAW.Models/Entities/Comments.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAW.Models.Entities
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public string Text { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // Relation with Card
+        public int CardId { get; set; }
+        public Card Card { get; set; }
+    }
+}
